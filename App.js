90d88a52e87b6bf894641a5e9947483ffbe5f6f1
Rajat Luthra <t-rajat.luthra@ocltp.com>
2023-07-30 21:44:27 +0530

    EPISODE-03

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+/* 
 const heading = React.createElement(
   "h1",
   { id: "heading" },
@@ -8,3 +10,31 @@ const heading = React.createElement(
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
+this was the way to create react element using core react
+
+*/
+
+/*
+
+const jsxHeading = <h1 id="heading">Hello World from JSX!</h1>;
+This is way to create react element using JSX 
+if u console any of them jsxHeading or heading both will be object containing same things so therefore they are same, exactly same just the syntax is different 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
+
+*/
+
+// REACT COMPONENT
+
+const HeadingComponent = () => {
+  return (
+    <h1 id="container" className="heading">
+      Namaste React Functional Component
+    </h1>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />); // way of rendering functional component is also different.
