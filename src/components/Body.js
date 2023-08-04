6d27c0e6e2a8ec81ba92fadf1953bdaa2d94843d
Rajat Luthra <t-rajat.luthra@ocltp.com>
2023-08-05 00:49:45 +0530

    EPISODE-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,57 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  // if u dnt make filtered restaurants state so when you search once u will modify make list of restaurants and then if u search again u wont have all restuarants
+  //u will only have searched restaurants of before so u wont be able to search new restaurants, so we need to conserve the main list of restaurants and
+  //make another list of filtered list which has list all restaurants from api call.
+  const [searchText, setSearchText] = useState("");
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3603185&lng=76.44763209999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurants(
+      json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log(json);
+  };
+
+  return listOfRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurants = listOfRestaurants.filter((res) => {
+                return res.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+              });
+              setFilteredRestaurants(filteredRestaurants);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -33,10 +78,8 @@ const Body = () => {
   
           OLD METHOD
           */}
-        {listOfRestaurants.map((restaurant) => {
-          return (
-            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
-          );
+        {filteredRestaurants.map((restaurant) => {
+          return <RestaurantCard resData={restaurant} />;
         })}
 
         {/*
